5d70c96a9f1e4072037838af9dd35bffa82a1f23
phoenixstears <paruwkiberlinki1234@wp.pl>
2025-07-28 22:28:17 +0200

    finish work on credits

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -6,7 +6,7 @@ func _on_exit_pressed() -> void:
 
 
 func _on_credits_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/Credits.tscn")
 
 
 func _on_settings_pressed() -> void:
